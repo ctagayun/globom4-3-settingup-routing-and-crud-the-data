bc15be80d3ffeddab234d0f57b060ddb442d2c37
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-11-14 09:50:09 -0600

    Save

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -60,8 +61,59 @@ app.MapGet("/house/{houseId:int}", async(int houseId,IHouseRepository repo) =>
 
      //*Use the result object to determine to the status code to return.
      return Results.Ok(house);
+     //*This is the metadata forSwagger has to be added again so that it knows 
+     //*EP producess a 404 and 200OK
    }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
-                     
+
+  //*Add house
+  //*Tell the api to look for the dto in the body. this is the first parameter,
+  //*second parameter is the IHouseRepository
+  app.MapPost("/houses", async([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
+   {
+     var newHouse = await repo.Add(dto); //*now get the house
+
+     //*Use the result object to determine to the status code to return.
+     //*The first param by REST convention we return the URL and ID where the newly 
+     //*created house can be found. The second param is the house as it was added to the 
+     //*database
+     return Results.Created($"/houses/{newHouse.id}", newHouse);
+     //*This is the metadata forSwagger has to be added again so that it knows 
+     //*EP producess a 201
+   }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);          
+
+  //*Update house
+  //*Tell the api to look for the dto in the body. this is the first parameter,
+  //*second parameter is the IHouseRepository
+  app.MapPut("/houses", async([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
+   {
+     //*We need to make sure that the house in the request body actually exist 
+     if(await repo.Get(dto.Id) == null) 
+       return Results.Problem($"House with ID {dto.Id} not found.",
+              statusCode: 404);
+
+     var updatedHouse = await repo.Update(dto); //*now update the house
+    
+     return Results.Ok(updatedHouse);
+
+     //*This is the metadata forSwagger has to be added again so that it knows 
+     //*EP producess a 201
+   }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);        
+
+    //* Delete House
+    app.MapDelete("/houses/{houseId:int}", async(int houseId, IHouseRepository repo) =>
+   {
+     if(await repo.Get(houseId) == null) 
+       return Results.Problem($"House with ID {houseId} not found.",
+              statusCode: 404);
+
+     //*Note no date to return
+     await repo.Delete(houseId); //*now delete the house
+    
+     return Results.Ok();
+
+     //*This is the metadata forSwagger has to be added again so that it knows 
+     //*EP producess a 201
+   }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);      
 
 app.Run();
 
